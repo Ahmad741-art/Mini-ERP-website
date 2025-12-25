cc4b6771434953359e7db6fd74c91abb4496fd52
Ahmad741-art <155184460+Ahmad741-art@users.noreply.github.com>
2025-12-25 02:30:46 +0100

    in progress

--- Frontend/src/components/Layout/Layout.js
+++ Frontend/src/components/Layout/Layout.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Header from './Header';
 import Sidebar from './Sidebar';
-import './Layout.css';
+import './Layout.css'
 
 const Layout = ({ children }) => {
   return (
